92248f8af339e1dd00f5f73fbee2a937ea4d118e
Sanne Ypenburg <sanne.ypenburg@rivm.nl>
2022-05-23 17:56:53 +0200

    Added select_component module to server #7

--- server.R
+++ server.R
@@ -5,6 +5,7 @@ shinyServer(function(global, input, output, session) {
   # The dateRangeInput for date range selection 
   select_date_range <- date_range_server("select_date_range")
   
-
-
+  # The pickerInput for component selection 
+  select_component <- component_selection_server("select_component")
+  
 })
