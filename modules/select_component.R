bf729babd519ca17e37402621e6e73341e33a630
Sanne Ypenburg <sanne.ypenburg@rivm.nl>
2022-05-23 17:50:40 +0200

    Module for the component selection #7

---
+++ modules/select_component.R
@@ -0,0 +1,40 @@
+###############################################
+### pickerInput - select component ###
+###############################################
+
+# This is a component selection module
+######################################################################
+# Output Module
+######################################################################
+
+component_selection_output <- function(id) {
+  
+  ns <- NS(id)
+  
+  tagList(
+    
+    pickerInput(
+      ns("component"),
+      label    = "Select component",
+      choices  = c("PM10", "PM10 - calibrated", "PM2.5", "PM2.5 - calibrated"),
+      selected = "PM10",
+      width    = 250
+      )
+    
+  )
+  
+}
+
+
+######################################################################
+# Server Module
+######################################################################
+
+component_selection_server <- function(id) {
+  
+  moduleServer(id, 
+               
+               return(selected_component = reactive({input$component}))
+               
+               )
+}
