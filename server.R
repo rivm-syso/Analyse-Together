63a7e6b95599f48dd625b5bf552c2f0befc55e3c
Sanne Ypenburg <sanne.ypenburg@rivm.nl>
2022-06-07 13:20:04 +0200

    Removed wrong comma's.

--- server.R
+++ server.R
@@ -4,11 +4,11 @@ shinyServer(function(global, input, output, session) {
 
   
   # The dateRangeInput for date range selection 
-  select_date_range <- date_range_server("select_date_range"),
+  select_date_range <- date_range_server("select_date_range")
   
 
   # The pickerInput for component selection 
-  select_component <- component_selection_server("select_component", comp_choices),
+  select_component <- component_selection_server("select_component", comp_choices)
   
 
 
