e9d1f3faff6f68d4fbd2fc3da7619491e5d82bd6
Sanne Ypenburg <sanne.ypenburg@rivm.nl>
2022-06-23 14:59:43 +0200

    Translated labels to English and sourced plot_timeseries.R.

--- global.R
+++ global.R
@@ -26,6 +26,7 @@ library(highcharter)     # For charts
 library(leaflet)         # For maps
 library(DT)              # For tables
 library(plotly)          # For graphs
+library(latex2exp)       # For titles in graphs
 
 # Geo
 library(sf)
@@ -69,7 +70,7 @@ col_cat <- list('#ffb612','#42145f','#777c00','#007bc7','#673327','#e17000','#39
 col_cat <- rev(col_cat) # the saturated colours first
 
 # Component choices
-overview_component <- data.frame('component' = c(" ","pm10","pm10_kal","pm25","pm25_kal"), 'label'=c(" ","PM10","PM10 - gekalibreerd","PM2.5" ,"PM2.5 - gekalibreerd" ))
+overview_component <- data.frame('component' = c(" ","pm10","pm10_kal","pm25","pm25_kal"), 'label'=c(" ", "PM10","PM10 - calibrated","PM2.5" ,"PM2.5 - calibrated" ))
 comp_choices = setNames(overview_component$component, overview_component$label)
 
 
@@ -83,6 +84,7 @@ source("modules/select_date_range.R")
 source("modules/select_component.R")
 
 # Source modules visualisation
+source("modules/plot_timeseries.R")
 
 # Source functions
 source("funs/assign_color_stations.R")
