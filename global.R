46885bbbd15f39fc6d5cafe84d2d9cd7d8da57d3
spijker <job.spijker@rivm.nl>
2022-07-06 13:43:56 +0200

    Reorganized the code in global.R

--- global.R
+++ global.R
@@ -58,23 +58,13 @@ Sys.setlocale("LC_TIME", 'dutch')            # Dutch date format
 Sys.setlocale('LC_CTYPE', 'en_US.UTF-8')     # Dutch CTYPE format
 
 
-# Connect with the database using pool                                      ====
+# Connect with the database using pool, store data, read table              ====
 pool <- dbPool(
 
   drv = SQLite(),
   dbname = datafile("database.db")
 
 )
-
-# Colours for the sensors
-col_cat <- list('#ffb612','#42145f','#777c00','#007bc7','#673327','#e17000','#39870c', '#94710a','#01689b','#f9e11e','#76d2b6','#d52b1e','#8fcae7','#ca005d','#275937','#f092cd')
-col_default <- '#000000'
-col_overload <- '#111111'
-
-# Linetype for the reference stations
-line_cat <- list('dashed', 'dotted', 'dotdash', 'longdash', 'twodash')
-line_default <- 'solid'
-line_overload <- 'dotted'
 
 # store lists with projects and municipalities
 municipalities <- read_csv("./prepped_data/municipalities.csv")
@@ -86,18 +76,28 @@ add_doc("application", "municipalities", municipalities, conn = pool,
 add_doc("application", "projects", projects, conn = pool, 
         overwrite = TRUE)
 
-
-# Read out the database to dataframes
 measurements <- tbl(pool, "measurements") %>% as.data.frame() %>% mutate(date = lubridate::as_datetime(timestamp, tz = "Europe/Amsterdam"))
 sensor <- tbl(pool, "location") %>% as.data.frame() %>% mutate(selected = F, col = col_default, linetype = line_default, station_type = "sensor")
 
+
+
+# Define colors, line types,choices etc.                                   ====
 # Colours for the sensors
 col_cat <- list('#ffb612','#42145f','#777c00','#007bc7','#673327','#e17000','#39870c', '#94710a','#01689b','#f9e11e','#76d2b6','#d52b1e','#8fcae7','#ca005d','#275937','#f092cd')
 col_cat <- rev(col_cat) # the saturated colours first
+col_default <- '#000000'
+col_overload <- '#111111'
 
 # Component choices
 overview_component <- data.frame('component' = c(" ","pm10","pm10_kal","pm25","pm25_kal"), 'label'=c(" ", "PM10","PM10 - calibrated","PM2.5" ,"PM2.5 - calibrated" ))
 comp_choices = setNames(overview_component$component, overview_component$label)
+
+# Linetype for the reference stations
+line_cat <- list('dashed', 'dotted', 'dotdash', 'longdash', 'twodash')
+line_default <- 'solid'
+line_overload <- 'dotted'
+
+
 
 
 ### APP SPECIFIC SETTINGS                                                   ====
