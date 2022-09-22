8c074af636bd5eb6164f6377f6a8b443070432d1
Job Spijker <job.spijker@rivm.nl>
2022-09-22 09:23:03 +0200

    Rewrote download section into function, #61

--- scripts/download_demo_database.R
+++ scripts/download_demo_database.R
@@ -67,6 +67,101 @@ station_exists <- function(station, conn) {
 
 
 
+download_kits_data <- function(kits, time_start, time_end, pool) {
+    # This function download sensor, meteo and air quality reference station data.
+    # Based on the sensor meta data, meteo and reference data stations
+    # are selected. For these stations the data is also downloaded. At
+    # the end of the function, the locations of the meteo and ref.
+    # station are stored in the db, if they don't allready exists
+    # 
+    # Please note, this function expects that meta data of the sensor
+    # is allready present in the data base
+    #
+    # arguments:
+    # kits: vector of kit id's
+    # time_start, time_end: datetime objects with start and end date
+    # pool: db connection object
+
+    # some variables to use as counter and  store stationcodes
+    counter <- 1
+    knmicodes <- c()
+    lmlcodes <- c()
+
+    # for each kit ...
+    for(i in kits) {
+
+        # Download data
+        log_debug("downloading measurements for station {i}, {counter}/{length(kits)}")
+        date_range <- round_to_days(time_start, time_end)
+        d <- download_data(i, Tstart = time_start, Tend = time_end,
+                           fun = "download_data_samenmeten",
+                           conn = pool)
+        log_trace("got {nrow(d)} measurements")
+        counter <- counter + 1
+
+        # get meta data for kit, to get meteo and AQ station id's
+        kitmeta <- get_doc(type = "station", ref = i, conn = pool)
+
+        # Download meteo station data
+        log_debug("downloading measurements for station {i}, {counter}/{length(kits)}")
+        date_range <- round_to_days(time_start, time_end)
+        knmistation <- kitmeta %>% 
+            select(knmicode)  %>%
+            mutate(knmi_id = str_replace(knmicode, "knmi_06", "KNMI_")) %>%
+            pull(knmi_id)
+        knmicodes <- c(knmicodes, knmistation)
+        d <- download_data(knmistation, Tstart = time_start, Tend = time_end,
+                           fun = "download_data_knmi",
+                           conn = pool)
+        log_trace("got {nrow(d)} of KNMI measurements")
+
+        # download AQ reference station data
+        #
+        # !!!!! are all parameters of the LML station downloaded?
+        # CHECK!!!!
+        #
+        lmlstation <- kitmeta %>% 
+            pull(pm10closecode)
+        lmlcodes <- c(lmlcodes, lmlstation)
+        d <- download_data(lmlstation, Tstart = time_start, Tend = time_end,
+                           fun = "download_data_lml",
+                           conn = pool)
+
+    }
+
+
+    # store locations KNMI stations
+    knmi_stations_locations <- download_locations_knmi(unique(knmicodes), 
+                                                       time_start, time_end)
+    for (i in unique(knmicodes)) {
+        log_trace("checking location for KNMI station  {i}")
+        if(!station_exists(i, conn = pool)) {
+            log_trace("download and store location for KNMI station  {i}")
+            loc <- download_locations_knmi(i, time_start, time_end)
+            insert_location_info(station = loc[1,1],
+                                 lat = loc[1,2],
+                                 lon = loc[1,3],
+                                 conn = pool)
+        } 
+
+    }
+
+
+    # store locations LML stations
+    for (i in unique(lmlcodes)) {
+        log_trace("checking location for LML station  {i}")
+        if(!station_exists(i, conn = pool)) {
+            log_trace("download an store  location for LML station  {i}")
+            loc <- download_locations_lml(i)
+            insert_location_info(station = loc[1, 1],
+                                 lat = loc[1, 2],
+                                 lon = loc[1, 3],
+                                 conn = pool)
+        }
+
+    }
+}
+
 # Connect with the database using pool, store data, read table              ====
 pool <- dbPool(
 
@@ -79,7 +174,7 @@ pool <- dbPool(
 project <- "Amersfoort"
 
 time_start <- as_datetime("2022-01-01 00:00:00")
-time_end <- as_datetime("2022-01-06 23:59:59")
+time_end <- as_datetime("2022-01-07 23:59:59")
 
 
 #  get project info
@@ -88,86 +183,7 @@ download_project(project)
 # get which kits are part of the project
 kits <- get_stations_from_project(project)
 
+donwload_kits_data(kits, time_start, time_end, pool)
 
 
-# downlod sensor data
-counter <- 1
-for(i in kits) {
-    log_debug("downloading measurements for station {i}, {counter}/{length(kits)}")
-    date_range <- round_to_days(time_start, time_end)
-    d <- download_data(i, Tstart = time_start, Tend = time_end,
-                       fun = "download_data_samenmeten",
-                       conn = pool)
-    log_trace("got {nrow(d)} measurements")
-    counter <- counter + 1
-}
-
-
-# download KNMI and LML data, collect station ids so we can get the
-# locations of the stations later on.
-knmicodes <- c()
-lmlcodes <- c()
-for(i in kits) {
-    log_debug("downloading measurements for station {i}, {counter}/{length(kits)}")
-    date_range <- round_to_days(time_start, time_end)
-    kitmeta <- get_doc(type = "station", ref = i, conn = pool)
-    knmistation <- kitmeta %>% 
-        select(knmicode)  %>%
-        mutate(knmi_id = str_replace(knmicode, "knmi_06", "KNMI_")) %>%
-        pull(knmi_id)
-
-    knmicodes <- c(knmicodes, knmistation)
-
-    d <- download_data(knmistation, Tstart = time_start, Tend = time_end,
-                       fun = "download_data_knmi",
-                       conn = pool)
-
-    log_trace("got {nrow(d)} of KNMI measurements")
-
-    lmlstation <- kitmeta %>% 
-        pull(pm10closecode)
-
-    lmlcodes <- c(lmlcodes, lmlstation)
-
-    d <- download_data(lmlstation, Tstart = time_start, Tend = time_end,
-                       fun = "download_data_lml",
-                       conn = pool)
-
-}
-
-
-# store locations KNMI stations
-knmi_stations_locations <- download_locations_knmi(unique(knmicodes), time_start, time_end)
-for (i in unique(knmicodes)) {
-
-    log_trace("checking location for KNMI station  {i}")
-
-    if(!station_exists(i, conn = pool)) {
-        log_trace("download and store location for KNMI station  {i}")
-        loc <- download_locations_knmi(i, time_start, time_end)
-        insert_location_info(station = loc[1,1],
-                             lat = loc[1,2],
-                             lon = loc[1,3],
-                             conn = pool)
-    } 
-
-}
-
-
-# store locations LML stations
-for (i in unique(lmlcodes)) {
-
-    log_trace("checking location for LML station  {i}")
-
-    if(!station_exists(i, conn = pool)) {
-    log_trace("download an store  location for LML station  {i}")
-        loc <- download_locations_lml(i)
-
-        insert_location_info(station = loc[1, 1],
-                             lat = loc[1, 2],
-                             lon = loc[1, 3],
-                             conn = pool)
-    }
-
-}
 
