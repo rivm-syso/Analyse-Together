2936a962e8c5b1b2958aec108ad54bdcd2098768
tennere <elma.tenner@rivm.nl>
2022-09-27 10:15:30 +0200

    #72, the theme for the plots in the global and as input for the plot modules. Changed the names of the modules to the same format.

--- modules/add_timeseries_plot.R
+++ modules/add_timeseries_plot.R
@@ -9,9 +9,9 @@
 #################################################################
 
 timeseries_output <- function(id) {
-  
+
   ns <- NS(id)
-  
+
   plotOutput(ns("timeseries_plot"), hover = hoverOpts(id ="plot_hover"))
 
 }
@@ -21,12 +21,15 @@ timeseries_output <- function(id) {
 # Server Module
 ######################################################################
 
-timeseries_server <- function(id, data_measurements_stations, overview_component) {
-  
+timeseries_server <- function(id,
+                              data_measurements_stations,
+                              overview_component,
+                              theme_plots) {
+
   moduleServer(id, function(input, output, session) {
-                 
+
                  ns <- session$ns
-                 
+
                  # Get selected measurements from communication module
                  data_measurements <- reactive({
                    data_measurements <- data_measurements_stations$selected_measurements()
@@ -38,13 +41,13 @@ timeseries_server <- function(id, data_measurements_stations, overview_component
                    data_stations <- data_measurements_stations$station_locations()
                    return(data_stations)
                    })
-                 
-                 # Create time plot with ggplot 
+
+                 # Create time plot with ggplot
                  output$timeseries_plot <- renderPlot({
-                   
+
                    # Determine parameter that needs to be plotted
                    parameter <- data_measurements()$parameter
-                   
+
                    # Find the corresponding label
                    parameter_label <- filter(overview_component, component == parameter[1])['label']
                    if (nrow(parameter_label) < 1){
@@ -52,7 +55,7 @@ timeseries_server <- function(id, data_measurements_stations, overview_component
                    }
                    # Ad colour and linetype to the data_measurements
                    data_timeseries <- data_measurements() %>% left_join(select(data_stations(), c(station, col, linetype, size, station_type)), by = "station")
-                   
+
                    # Calculate standard deviation
                    data_timeseries <- data_timeseries %>% group_by(station) %>% mutate(sd = sd(value)) %>% ungroup()
                    n_days_in_plot <- round(as.numeric(max(data_timeseries$date) - min(data_timeseries$date)))
@@ -60,27 +63,13 @@ timeseries_server <- function(id, data_measurements_stations, overview_component
                    min_meas <- plyr::round_any(min(data_timeseries$value), 5, f = floor)
                    max_meas <- plyr::round_any(max(data_timeseries$value), 5, f = ceiling)
                    steps <- plyr::round_any(max_meas / 15, 10, f = ceiling) # to create interactive y-breaks
-                 
-                   theme_plots <- theme_bw(base_size = 18) + 
-                     theme(strip.text.x = element_text(size = 14, colour = "black"),
-                           axis.text.y = element_text(face = "bold",color = "black", size = 16),
-                           axis.text.x = element_text(color = "black", size = 16),
-                           axis.title = element_text(color = "black", size = 16),
-                           text = element_text(family = 'serif'),
-                           title = element_text(color = "black", size = 16),
-                           legend.title = element_text(size = 16),
-                           legend.text = element_text(size = paste0(16-log(n_stat_in_plot)*2)),
-                           legend.key.height = unit(0.5, 'cm'),
-                           legend.key.width = unit(0.5, 'cm'),
-                           panel.border = element_rect(colour = "black", fill=NA, size=1))
-                   
-                   
+
                    # Make a plot
                    if (length(parameter>0)){
                        try(ggplot(data = data_timeseries, aes(x = date, y = value, group = station)) +
                          geom_line(aes(color = station, linetype=station_type)) +
                          geom_ribbon(aes(y = value, ymin = value - sd, ymax = value + sd, fill = station), alpha = .2) +
-                         scale_color_manual(values=c(paste0(data_timeseries$col)),
+                         scale_color_manual(values = c(paste0(data_timeseries$col)),
                                             breaks = c(paste0(data_timeseries$station))) +
                          scale_fill_manual(values=c(paste0(data_timeseries$col)),
                                            breaks = c(paste0(data_timeseries$station))) +
@@ -90,17 +79,15 @@ timeseries_server <- function(id, data_measurements_stations, overview_component
                          scale_y_continuous(breaks = seq(min_meas-steps,max_meas+steps, by = steps), minor_breaks = seq(min_meas-(steps/2),max_meas+(steps/2), by = steps/2), limits = c(min_meas-(steps/2), max_meas+(steps/2))) +
                          labs(x = "Date", y = expression(paste("Concentration (", mu, "g/",m^3,")")), title=paste0('Timeseries for: ', parameter_label)) +
                          expand_limits(y=0) +
-                         theme_bw() + 
+                         theme_plots +
+                         theme(legend.text = element_text(size = paste0(16-log(n_stat_in_plot)*2)))  +
                          guides(colour = guide_legend(override.aes = list(size=2)),
-                                linetype = guide_legend(override.aes = list(size = 1))) +
-
-                         theme_plots)
+                                linetype = guide_legend(override.aes = list(size = 1)))
+                       )
                      }
-                   
-                 })
-                 
-               })
-  
-  
-}
 
+                 })
+
+               })
+
+}
