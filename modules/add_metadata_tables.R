8e47df5e5352568692fcb8b3592bcc7168e0d2ad
hendricw <wouter.hendricx@rivm.nl>
2022-11-01 16:40:01 +0100

    Now all the data points are included in the meta-data table, without regarding the selection of stations. #114

--- modules/add_metadata_tables.R
+++ modules/add_metadata_tables.R
@@ -30,27 +30,39 @@ metadata_server <- function(id, com_module) {
     
     # Get selected measurements from communication module
     metadata_table <- reactive({
-
-      shiny::validate(need(nrow(com_module$selected_measurements()) >0, message =  "Select one or more sensors (with data)"))
-      data_measurements <- com_module$selected_measurements()
-
+      
+      #shiny::validate(need(nrow(com_module$selected_measurements()) >0, message =  "Select one or more sensors (with data)"))
+      data_measurements <- com_module$all_measurements()
+      
       timerange <- difftime(com_module$selected_time()$end_time, com_module$selected_time()$start_time, units="hours")
-      metadata_table <- data_measurements %>%  group_by(station) %>%
-                        mutate(n_obs = n(),
-                               max_obs = timerange,
-                               first_m = as.POSIXct(as.numeric(min(timestamp)), origin='1970-01-01') %>% format(., "%d %b %Y"),
-                               last_m = as.POSIXct(as.numeric(max(timestamp)), origin='1970-01-01') %>% format(., "%d %b %Y")) %>%
-                        select(station, max_obs, n_obs, first_m, last_m) %>%
-                        distinct(station, .keep_all = T)
+      if (nrow(data_measurements)>0){
+          metadata_table <- data_measurements %>%  
+                            dplyr::group_by(station) %>% 
+                            dplyr::mutate(n_obs = n(),
+                                          max_obs = timerange,
+                                          first_m = as.POSIXct(as.numeric(min(timestamp)), origin='1970-01-01') %>% format(., "%d %b %Y"),
+                                          last_m = as.POSIXct(as.numeric(max(timestamp)), origin='1970-01-01') %>% format(., "%d %b %Y")) %>%
+                            dplyr::select(station, max_obs, n_obs, first_m, last_m) %>%
+                            dplyr::distinct(station, .keep_all = T)
+      }
+      else{
+        metadata_table <- data_measurements %>% 
+          dplyr::mutate(n_obs = NA,
+                        max_obs = timerange,
+                        first_m = NA,
+                        last_m = NA) %>%
+          dplyr::select(station, max_obs, n_obs, first_m, last_m) %>%
+          dplyr::distinct(station, .keep_all = T)
+      }
 
       return(metadata_table)
     })
     
     # Get selected stations from communication module
     data_merged <- reactive({
-
+      
       data_stations <- com_module$station_locations() %>% select(c(station, lat, lon, station_type)) %>% dplyr::distinct(station, .keep_all = T)
-      data_merged <- left_join(metadata_table(),data_stations, by = "station")
+      data_merged <- left_join(data_stations,metadata_table(), by = "station")
       
       return(data_merged)
     })
@@ -59,7 +71,6 @@ metadata_server <- function(id, com_module) {
       project_or_municipality <- com_module$mun_proj_select()
       return(project_or_municipality)
     })
-    
     
     
     breaks_col <- reactive({
@@ -75,11 +86,19 @@ metadata_server <- function(id, com_module) {
       renderDataTable({
 
         # Determine parameter that needs to be plotted
-        n_obs_sel <- metadata_table()$n_obs
+        n_obs_sel <- data_merged()$station
         
         if(length(n_obs_sel>1)){
-          try(datatable(data_merged(),colnames = c("Number of measurements" = "n_obs", "Maximum measurements" = "max_obs", "First measurements" = "first_m", "Last measurements" = "last_m", "Type" = "station_type", "Latitude" = "lat", "Longitude" = "lon"),
-                        caption = paste0(i18n$t("word_table")," ",unique(com_module$selected_measurements()$parameter),","," ", i18n$t("word_within"), " ",project_or_municipality()),options = list(scrollX = TRUE)) %>%
+          try(datatable(data_merged(),colnames = c("Number of measurements" = "n_obs", 
+                                                   "Maximum measurements" = "max_obs", 
+                                                   "First measurements" = "first_m", 
+                                                   "Last measurements" = "last_m", 
+                                                   "Type" = "station_type", 
+                                                   "Latitude" = "lat", 
+                                                   "Longitude" = "lon"),
+                        caption = paste0(i18n$t("word_table")," ",unique(com_module$selected_measurements()$parameter),
+                                         ","," ", i18n$t("word_within"), " ",project_or_municipality()),
+                        options = list(scrollX = TRUE, pageLength = 10, lengthChange = FALSE)) %>%
                 formatStyle("Number of measurements", backgroundColor = styleInterval(cuts = breaks_col()[[1]], values = breaks_col()[[2]]))
           )
         }})      
