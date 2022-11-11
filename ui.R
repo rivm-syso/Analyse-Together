648005e2c41f51193c82689cd98da400a98f72c0
hendricw <wouter.hendricx@rivm.nl>
2022-11-11 14:30:35 +0100

    fixed map and button locations update-data and language.

--- ui.R
+++ ui.R
@@ -16,7 +16,7 @@ shinyUI(
       # White header with logo
       tags$div(class = "header-white",
                
-               tags$img(src = "images/Banner_2018.png", style = "height: 120px; width: 450px; margin-bottom: 0px;margin-top: 10px; margin-right: 10px")
+               tags$img(src = "images/Banner_2018.png", style = "height: 120px; width: 450px; margin-bottom: 0px;margin-top: 0px; margin-right: 10px")
                
       ),
       
@@ -26,7 +26,7 @@ shinyUI(
                tags$style(HTML("h1 {margin-bottom: -15px;")),
                
                tags$h1("Samen Analyseren"),
-               tags$h4(paste("Version", application_version)),
+               tags$h4(paste("Version", application_version), style = "margin-bottom: -10px"),
                tags$p("")
       )
       
@@ -39,21 +39,17 @@ shinyUI(
     tabPanel(
       title = "Home",
       
-      fluidRow(
-        column(width = 2,offset = 8,
-               shiny.i18n::usei18n(i18n),
-               radioGroupButtons('selected_language', size = 'sm',justified = T,width = '120px',
-                                 label = i18n$t("sel_language"),
-                                 choices = i18n$get_languages()[!i18n$get_languages() %in% grep("tag", i18n$get_languages(), value = T)],
-                                 selected = i18n$get_key_translation(), direction = 'horizontal'),
-               align = "center",
-               style = "margin-bottom: 10px;",
-               style = "margin-top: -10px;"),
-        conditionalPanel(condition="input.second_order_tabs!='Select data'",
-                         column(width = 1, update_data_button_output("update_data"),                
-                                align = "center", style = "margin-left: -10px;margin-bottom: 10px;margin-top: 10px;margin-right:10px;"))
-        ),
-      
+      fluidRow(column(width = 1, offset=10,
+             shiny.i18n::usei18n(i18n),
+             radioGroupButtons('selected_language', size = 'sm',justified = T,width = '80px',
+                               label = NULL,
+                               choices = i18n$get_languages()[!i18n$get_languages() %in% grep("tag", i18n$get_languages(), value = T)],
+                               selected = i18n$get_key_translation(), direction = 'horizontal'),
+             align = "left",
+             style = "margin-bottom: -10px;",
+             style = "margin-right: 20px;",
+             style = "margin-top: -10px;")),
+    
       fluidRow(
         column(width = 6,
                
@@ -69,10 +65,10 @@ shinyUI(
                              fluidRow(
                                
                                column(12, class = "col-lg-12", wellPanel(project_or_mun_selection_output("proj_or_mun_select"),
-                                                                       choice_selection_output("choice_select"),style = "z-index: 10;",
+                                                                       choice_selection_output("choice_select"),
                                                                        
-                                                                       date_range_output("select_date_range"),style = "z-index: 11;",
-                                                                       download_api_button_output("dl_btn_pushed"),style = "z-index: 11;"
+                                                                       date_range_output("select_date_range"),
+                                                                       download_api_button_output("dl_btn_pushed")
                                )
                                )
                                # ,
@@ -113,17 +109,22 @@ shinyUI(
                            )
                )
         ),
+
         # Add all text:
+    
+        conditionalPanel(condition="input.second_order_tabs!='Select data'",
+                         column(width = 1, update_data_button_output("update_data"),                
+                                align = "center", style = "margin-left: -10px;margin-top: -10px; margin-bottom: -10px; margin-right:-10px;")),
         conditionalPanel(condition="input.second_order_tabs=='Select data'", column(width = 6,div(br(),br(),h3(i18n$t("tool_welcome")),
                                                                                                   p(i18n$t("tool_welcome_expl")),
                                                                                                   p(i18n$t("link_to_samenmeten"), a("samenmeten.rivm.nl", href ='https://samenmeten.rivm.nl/dataportaal/', target = 'blank'),
                                                                                                     br(),i18n$t("link_to_LML"), a("luchtmeetnet.nl", href ='https://www.luchtmeetnet.nl/', target = 'blank'),
-                                                                                                    br(),i18n$t("link_to_projecten"), a("samenmeten.nl", href ='https://samenmeten.nl/projecten', target = 'blank'), style = "font-size:13px"), style='text-align: left;'))),
-        conditionalPanel(condition="input.second_order_tabs=='Informatie over data'", column(width = 6,div(br(),br(),i18n$t("expl_metadata")))),
-        conditionalPanel(condition="input.second_order_tabs=='Visualise data'", column(width = 6,  show_map_output("map"))),
-        
-      )
-    ),
+                                                                                                    br(),i18n$t("link_to_projecten"), a("samenmeten.nl", href ='https://samenmeten.nl/projecten', target = 'blank'), style = "font-size:13px"), style='text-align: left;margin-top: -10px;'))),
+        conditionalPanel(condition="input.second_order_tabs=='Informatie over data'", column(width = 6,div(br(),i18n$t("expl_metadata")))),
+        conditionalPanel(condition="input.second_order_tabs=='Visualise data'", column(width = 6,  show_map_output("map"),style = "margin-top: 30px;")),
+          
+        )
+      ),
     
     tabPanel(
       title = i18n$t("title_infotool"),
