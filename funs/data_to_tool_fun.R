91360faf11294ca25c38a48086a39052b9dac844
tennere <elma.tenner@rivm.nl>
2022-12-06 12:05:14 +0100

    Some functions to enhance the data

---
+++ funs/data_to_tool_fun.R
@@ -0,0 +1,87 @@
+# misc functions to get data from the database and in the format used in the tool
+
+get_measurements <- function(measurements_con, stations_name, start_time, end_time){
+  # Function to get all the measurements from the stations in the period
+  # arguments:
+  #    measurements_con:
+  #    stations_name:
+  #    start_time:
+  #    end_time:
+  #
+  # return: dataframe
+
+  # Get the data measurements of the selected Municipality/project
+  data_measurements <- measurements_con %>%
+    dplyr::filter(station %in% stations_name) %>%
+    as.data.frame()
+
+  # Filter the measurements on time
+  data_measurements <- data_measurements %>%
+    dplyr::mutate(date = lubridate::as_datetime(timestamp, tz = "Europe/Amsterdam")) %>%
+    dplyr::filter(date > start_time &
+                    date < end_time)
+
+  return(data_measurements)
+}
+
+get_locations <- function(stations_con, stations_name){
+  # Function to get the location and metadata from the stations
+  # arguments:
+  #    stations_con:
+  #    stations_name:
+  #
+  # return: dataframe
+
+  # Get the information from the stations
+  data_locations <- stations_con %>%
+    dplyr::filter(station %in% stations_name) %>%
+    as.data.frame()
+
+  return(data_locations)
+}
+
+add_ref_kal <- function(data_measurements){
+  # Function to add a pm10_kal and pm25_kal value to the reference stations. In
+  # the Netherlands the sensor do have a calibrated value, for the reference
+  # stations the normal value is also set as calibrated value.
+  # argument:
+  #   data_measurements: dataframe with the measurements with at least those
+  #                     columns (parameter, value, station, date)
+  # return: same dataframs as data_measurements
+
+  # Create a pm10_kal and pm25_kl for reference stations
+  cols_pivot <- data_measurements %>% dplyr::pull(parameter) %>% unique()
+  data_measurements <- data_measurements %>%
+    tidyr::pivot_wider(names_from = parameter, values_from = value) %>%
+    dplyr::mutate(pm10_kal = dplyr::case_when(grepl("NL", station) ~ pm10,
+                                              T ~ pm10_kal),
+                  pm25_kal = dplyr::case_when(grepl("NL", station) ~ pm25,
+                                              T ~ pm25_kal)) %>%
+    tidyr::pivot_longer(cols = cols_pivot, names_to = "parameter", values_to = "value") %>%
+    dplyr::group_by(station, date, parameter) %>%
+    dplyr::slice(which.max(!is.na(value))) %>%
+    dplyr::ungroup()
+
+  #retrun
+  return(data_measurements)
+}
+
+add_uncertainty_sensor <- function(data_measurements, uc_pm10 = 8.5, uc_pm25 = 5.3){
+  # Function to add a value of uncertainty to the sensor data,
+  # default values from research Boeren en Buren.
+  # arguments:
+  #   data_measurements: dataframe
+  #   uc_pm10: double, to indicate the uncertainty in pm10, default = 8.5
+  #   uc_pm25: double, to indicate the uncertainty in pm25, default = 5.3
+  # return: dataframe as data_measurements with extra column "sd",
+  # this column is 0 for reference stations
+
+  # Add uncertainty to the measurements of the sensors
+  data_measurements <- data_measurements %>%
+    dplyr::mutate(sd = dplyr::case_when(parameter == "pm25_kal" & !grepl("NL", station) ~ uc_pm25,
+                                        parameter == "pm25" & !grepl("NL", station) ~ uc_pm25,
+                                        parameter == "pm10_kal" & !grepl("NL", station) ~ uc_pm10,
+                                        parameter == "pm10" & !grepl("NL", station) ~ uc_pm10,
+                                        T ~ 0))
+  return(data_measurements)
+}
