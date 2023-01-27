0521dbbfa88d9b7a09f7bb1578259645ed09368d
tennere <elma.tenner@rivm.nl>
2023-01-27 15:35:12 +0100

    #123, changed plot timeseries to display group

--- modules/add_timeseries_plot.R
+++ modules/add_timeseries_plot.R
@@ -34,7 +34,7 @@ timeseries_server <- function(id,
 
          # Create time plot with ggplot
          output$timeseries_plot <- renderPlot({
-           
+
            # Get the data to plot
            data_plot <- data_measurements$data_filtered
 
@@ -46,7 +46,7 @@ timeseries_server <- function(id,
 
          # Get the colours for the stations
          data_stations <- data_stations$data %>%
-           dplyr::select(c(station, col, linetype, size, station_type)) %>%
+           dplyr::select(c(station, col, linetype, size, station_type, label)) %>%
            dplyr::distinct(station, .keep_all = T) %>%
            dplyr::filter(!grepl("KNMI", station))
 
@@ -75,18 +75,19 @@ timeseries_server <- function(id,
          steps <- plyr::round_any(max_meas / 15, 10, f = ceiling) # to create interactive y-breaks
 
          # Make a plot ====
-         try(ggplot(data = data_timeseries, aes(x = date, y = value, group = station)) +
-               geom_line(aes(color = station, linetype=station_type)) +
-               geom_ribbon(aes(y = value, ymin = ribbon_min, ymax = ribbon_max, fill = station), alpha = .2) +
+         try(ggplot(data = data_timeseries, aes(x = date, y = value, group = label)) +
+               geom_line(aes(color = label, linetype = station_type)) +
+               geom_ribbon(aes(y = value, ymin = ribbon_min, ymax = ribbon_max, fill = label), alpha = .2) +
                scale_color_manual(values = c(paste0(data_timeseries$col)),
-                                  breaks = c(paste0(data_timeseries$station))) +
+                                  breaks = c(paste0(data_timeseries$label))) +
                scale_fill_manual(values=c(paste0(data_timeseries$col)),
-                                 breaks = c(paste0(data_timeseries$station))) +
+                                 breaks = c(paste0(data_timeseries$label))) +
                scale_size_manual(values = c(paste0(data_timeseries$station_type)),
                                  breaks = c(paste0(data_timeseries$size)), guide = 'none') +
                scale_x_datetime(date_breaks = paste0(as.character(dplyr::case_when(n_days_in_plot < 8 ~ 1, T ~ n_days_in_plot/7))," day"), date_minor_breaks = "1 day") +
                scale_y_continuous(breaks = seq(min_meas - steps, max_meas + steps, by = steps), minor_breaks = seq(min_meas - (steps/2), max_meas + (steps/2), by = steps/2), limits = c(0, max_meas + (steps/2))) +
-               labs(x = "Date", y = expression(paste("Concentration (", mu, "g/",m^3,")")), title=paste0('Timeseries for: ', parameter_label)) +
+               labs(x = "Date", y = expression(paste("Concentration (", mu, "g/",m^3,")")),
+                    title=paste0('Timeseries for: ', parameter_label)) +
                theme_plots +
                theme(legend.text = element_text(size = paste0(16-log(n_stat_in_plot)*2)))  +
                guides(colour = guide_legend(override.aes = list(size=2)),
