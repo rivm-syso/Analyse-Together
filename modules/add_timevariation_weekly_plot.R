a3d20704ee135929d45aa919a5bbc36392de3351
hendricw <wouter.hendricx@rivm.nl>
2023-02-14 16:03:07 +0100

    xlabels are in alphabetical order, instead of weekly cycle. #issue153

--- modules/add_timevariation_weekly_plot.R
+++ modules/add_timevariation_weekly_plot.R
@@ -49,22 +49,35 @@ timevar_weekly_server <- function(id,
 
 
       # Make a plot
-      plot_all <- timeVariation(data_plot,
-                    pollutant = "value",
-                    normalise = FALSE,
-                    group = "label",
-                    alpha = 0.1,
-                    cols = data_plot$col,
-                    local.tz = "Europe/Amsterdam",
-                    ylim = c(0,NA),
-                    ylab = parameter_label,
-                    start.day = 1,
-                    par.settings = list(fontsize=list(text=15)),
-                    key = T
-                    )
-
-      # Get a part of the plot - weekly
-      plot_all[[1]][1]
+      plot_all <- data_plot %>% dplyr::mutate(daynumber = weekdays(date)) %>%
+                                dplyr::group_by(label, daynumber) %>%
+                                dplyr::mutate(mean_day = mean(value, na.rm = T))
+      
+      # Obtain info for the axis
+      min_meas <- plyr::round_any(min(plot_all$mean_day, na.rm = T), 5, f = floor)
+      max_meas <- plyr::round_any(max(plot_all$mean_day, na.rm = T), 5, f = ceiling)
+      steps <- plyr::round_any(max_meas / 15, 6, f = ceiling) # to create interactive y-breaks
+      n_stat_in_plot <- length(unique(plot_all$col))
+      
+      plot_part <- ggplot(data = plot_all) +
+        geom_line(aes(x = daynumber, y = mean_day, group = label, color = label), lwd = 1) +
+        scale_color_manual(values = plot_all$col,
+                           breaks = plot_all$label) +
+        scale_y_continuous(breaks = seq(min_meas-steps,max_meas+steps, by = steps),
+                           minor_breaks = seq(min_meas-(steps/2),max_meas+(steps/2),
+                                              by = steps/2),
+                           limits = c(min_meas-(steps/2), max_meas+(steps/2))) +
+       labs(x = "Day of the week", y = expression(paste("Concentration (", mu, "g/",m^3,")")),
+             title=paste0('Daily pattern for: ', parameter_label)) +
+        expand_limits(y=0) +
+        theme_plots +
+        theme(legend.text = element_text(size = paste0(16-log(n_stat_in_plot)*2)),
+              axis.text.x = element_text(color = "black", size = 16, angle = 0,
+                                         hjust = 0.5, vjust = 0))  +
+        guides(colour = guide_legend(title = "Group / Station",
+                                     override.aes = list(size=3)))
+      
+      plot_part
 
     })
 
