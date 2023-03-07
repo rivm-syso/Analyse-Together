1322441c471630e68c1b80f236cd522d0bf7ed41
Hendricxw <69630498+Hendricxw@users.noreply.github.com>
2023-03-07 13:08:00 +0100

    Issue153 (#156)
    * xlabels are in alphabetical order, instead of weekly cycle. #issue153
    
    * converted openair function to dplyr functions #issue153
    
    * added translation for weekly and daily plots, but reactivity does not work yet for ggplots
    
    * added day and hournumber, similar to the openair package. #issue153
    
    * fixed hor. distance #issue153

--- modules/add_timevariation_weekly_plot.R
+++ modules/add_timevariation_weekly_plot.R
@@ -49,22 +49,44 @@ timevar_weekly_server <- function(id,
 
 
       # Make a plot
-      plot_all <- timeVariation(data_plot,
-                    pollutant = "value",
-                    normalise = FALSE,
-                    group = "label",
-                    alpha = 0.1,
-                    cols = data_plot$col,
-                    local.tz = "Europe/Amsterdam",
-                    ylim = c(0,NA),
-                    ylab = parameter_label,
-                    start.day = 1,
-                    par.settings = list(fontsize=list(text=15)),
-                    key = T
-                    )
-
-      # Get a part of the plot - weekly
-      plot_all[[1]][1]
+      plot_all <- data_plot %>% dplyr::mutate(daynumber = weekdays(date),
+                                              hourofday = hour(date)) %>%
+                                dplyr::group_by(label, daynumber, hourofday) %>%
+                                dplyr::mutate(mean_day_hour = mean(value, na.rm = T)) %>% 
+                                dplyr::arrange(date)
+      
+      plot_all$daynumber <- factor(plot_all$daynumber,levels = c("maandag", "dinsdag", "woensdag",
+                                                                 "donderdag", "vrijdag", "zaterdag", "zondag"))
+      
+      # Obtain info for the axis
+      min_meas <- plyr::round_any(min(plot_all$mean_day_hour, na.rm = T), 5, f = floor)
+      max_meas <- plyr::round_any(max(plot_all$mean_day_hour, na.rm = T), 5, f = ceiling)
+      steps <- plyr::round_any(max_meas / 15, 6, f = ceiling) # to create interactive y-breaks
+      n_stat_in_plot <- length(unique(plot_all$col))
+      
+      plot_part <- ggplot(data = plot_all,aes(x = hourofday, y = mean_day_hour, group = label, color = label), lwd = 1) +
+        geom_line() +
+        geom_point() +
+        facet_wrap(~ daynumber, nrow = 1) +
+        scale_color_manual(values = plot_all$col,
+                           breaks = plot_all$label) +
+        scale_y_continuous(breaks = seq(min_meas-steps,max_meas+steps, by = steps),
+                           minor_breaks = seq(min_meas-(steps/2),max_meas+(steps/2),
+                                              by = steps/2),
+                           limits = c(min_meas-(steps/2), max_meas+(steps/2))) +
+        scale_x_continuous(breaks = c(0, 6, 12, 18)) +
+        labs(x = element_blank(), y = expression(paste("Concentration (", mu, "g/",m^3,")")),
+             title=paste0('Weekly pattern for: ', parameter_label)) +
+        #expand_limits(y=0) +
+        theme_plots +
+        theme(legend.text = element_text(size = paste0(16-log(n_stat_in_plot)*2)),
+              axis.text.x = element_text(color = "black", size = 16, angle = 0,
+                                         hjust = 0.5, vjust = 0))  +
+        theme(panel.spacing.x = unit(0, "cm")) +
+        guides(colour = guide_legend(title = "Group / Station",
+                                     override.aes = list(size=3)))
+      
+      plot_part
 
     })
 
