26f576a17d0387155f3a447c232f4d9117785bf4
tennere <elma.tenner@rivm.nl>
2023-05-12 08:25:14 +0200

    #221, add new function to calculate the uncertainty of the sensor as a percentage of the value

--- funs/data_to_tool_fun.R
+++ funs/data_to_tool_fun.R
@@ -89,3 +89,32 @@ add_uncertainty_sensor <- function(data_measurements, uc_pm10 = 8.5, uc_pm25 = 5
                                         T ~ 0))
   return(data_measurements)
 }
+
+add_uncertainty_sensor_percent <- function(data_measurements, uc_pm10 = 20, uc_pm25 = 20, uc_min_pm10 = 8.5, uc_min_pm25 = 5.3){
+  # Function to add a percentage of the value as uncertainty to the sensor data, with
+  # a minimal uncertainty
+  # arguments:
+  #   data_measurements: dataframe
+  #   uc_pm10: double, to indicate the percentage uncertainty in pm10, default = 20
+  #   uc_pm25: double, to indicate the percentage uncertainty in pm25, default = 20
+  #   uc_pm10: double, to indicate the minimal uncertainty in pm10, default = 8.5
+  #   uc_pm25: double, to indicate the minimal uncertainty in pm25, default = 5.3
+  # return: dataframe as data_measurements with extra column "sd",
+  # this column is 0 for reference stations
+
+  # Add uncertainty to the measurements of the sensors
+  data_measurements <- data_measurements %>%
+    dplyr::mutate(sd = dplyr::case_when(parameter == "pm25_kal" & !grepl("NL", station) ~ uc_pm25/100*value,
+                                        parameter == "pm25" & !grepl("NL", station) ~ uc_pm25/100*value,
+                                        parameter == "pm10_kal" & !grepl("NL", station) ~ uc_pm10/100*value,
+                                        parameter == "pm10" & !grepl("NL", station) ~ uc_pm10/100*value,
+                                        T ~ 0))
+  # Check for the minimal uncertainty
+  data_measurements <- data_measurements %>%
+    dplyr::mutate(sd = dplyr::case_when(parameter == "pm25_kal" & !grepl("NL", station) & sd < uc_min_pm25 ~ uc_min_pm25,
+                                        parameter == "pm25" & !grepl("NL", station) & sd < uc_min_pm25 ~ uc_min_pm25,
+                                        parameter == "pm10_kal" & !grepl("NL", station) & sd < uc_min_pm10 ~ uc_min_pm10,
+                                        parameter == "pm10" & !grepl("NL", station) & sd < uc_min_pm10 ~ uc_min_pm10,
+                                        T ~ 0))
+  return(data_measurements)
+}
