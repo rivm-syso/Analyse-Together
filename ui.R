31c91a6ab347164a5524986bab9a7630ef61bd19
tennere <elma.tenner@rivm.nl>
2023-07-03 13:12:23 +0200

    #237, inlcuding link to info page samen meten

--- ui.R
+++ ui.R
@@ -189,6 +189,8 @@ shinyUI(
       h4(i18n$t("word_links")),
       p(i18n$t("expl_link_to_samenmeten"),
         a("samenmeten.rivm.nl", href ='https://samenmeten.rivm.nl/dataportaal/', target = 'blank'),
+        br(), i18n$t("expl_link_to_samenmeten_info"),
+        a("link", href ='https://samenmeten.nl/dataportaal/samen-analyseren-tool', target = 'blank'),
         br(), i18n$t("expl_link_github"),
         a("github", href ='https://github.com/rivm-syso/Analyse-Together', target = 'blank'),
         br(),i18n$t("expl_link_to_LML"),
