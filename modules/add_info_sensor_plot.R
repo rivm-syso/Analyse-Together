154e64d6dbbe5d813872e1dc066999fc7410d622
tennere <elma.tenner@rivm.nl>
2023-11-17 14:55:57 +0100

    #254, add dynamic legend to the start page calendar plot/sensor info, and only display sensors number

--- modules/add_info_sensor_plot.R
+++ modules/add_info_sensor_plot.R
@@ -40,26 +40,41 @@ info_sensor_server <- function(id,
              'Geen data beschikbaar')
       )
 
-      # Get the number of stations measuring pm
+      # Get the number of stations measuring pm and not NL-stations (ref stations)
       data_calender <- data_plot %>%
-        dplyr::filter(grepl("pm", parameter)) %>%
+        dplyr::filter(grepl("pm", parameter) & !grepl("NL", station)) %>%
         dplyr::select(c(station, date)) %>%
         unique() %>%
         dplyr::group_by(date) %>%
         dplyr::summarise(count_stations = n()) %>%
         dplyr::ungroup()
 
+      # Set the breaks
+      max_n_stations <- max(data_calender$count_stations)
+      breaks_values <- seq(from = 0, to = max_n_stations, length.out = 5) %>% round(0)
+      breaks_labels <- paste0(breaks_values[1:4], "-", breaks_values[2:5])
+
+      # If there are too few sensors for 5 categories, create less breaks
+      if(T %in% duplicated(breaks_values)){
+        breaks_values <- seq(from = 0, to = max_n_stations, by = 1)
+        nr_labels <- length(breaks_values)
+        breaks_labels <- paste0(breaks_values[1:nr_labels-1], "-", breaks_values[2:nr_labels])
+      }
+
+
       # Make a plot ====
       try(calendarPlot(data_calender, pollutant = "count_stations",
                        type = 'label',
                        local.tz = "Europe/Amsterdam",
                        cols = "Oranges",
-                       breaks = c(0,10,30,100, 800),
+                       breaks = breaks_values,
                        par.settings = list(fontsize=list(text=15)),
                        key.header = "Number of stations",
                        key.footer = '',
                        key.position = 'bottom',
-                       labels = c('0-10', '10-30', '30-100',  '100+')))
+                       labels = breaks_labels
+                       )
+                       )
 
     })
 
